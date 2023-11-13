85bb70729e3ff322d4eacba9646c2a41bc20d2b6
Kyle <kylehagain@gmail.com>
2023-11-12 21:05:56 -0500

    updates

---
+++ assignments/RecycleviewProject/app/src/main/java/com/example/recycleviewproject/Data.kt
@@ -0,0 +1,30 @@
+package com.example.recycleviewproject
+
+data class Data(val title: String, val imageResId: Int, val Text: String) {
+
+    private val titles = arrayOf("Chapter One",
+        "Chapter Two", "Chapter Three", "Chapter Four",
+        "Chapter Five", "Chapter Six", "Chapter Seven",
+        "Chapter Eight")
+    private val details = arrayOf("Item one details", "Item two details",
+        "Item three details", "Item four details",
+        "Item five details", "Item six details",
+        "Item seven details", "Item eight details")
+    private val images = intArrayOf(R.drawable.android_image_1,
+        R.drawable.android_image_2, R.drawable.android_image_3,
+        R.drawable.android_image_4, R.drawable.android_image_5,
+        R.drawable.android_image_6, R.drawable.android_image_7,
+        R.drawable.android_image_8)
+
+    fun getTitles(): Array<String> {
+        return titles
+    }
+
+    fun getDetails(): Array<String> {
+        return details
+    }
+
+    fun getImages(): IntArray {
+        return images
+    }
+}
---
+++ assignments/RecycleviewProject/app/src/main/java/com/example/recycleviewproject/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.recycleviewproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.recycleviewproject.databinding.ActivityMainBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        layoutManager = LinearLayoutManager(this)
+        binding.contentMain.recyclerView.layoutManager = layoutManager
+        adapter = RecyclerAdapter()
+        binding.contentMain.recyclerView.adapter = adapter
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
